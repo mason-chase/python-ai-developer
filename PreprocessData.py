848dff241637942025cad49e616da9e46f2d21e2
monirmo97 <monireh.moshavash@gmail.com>
2023-10-16 10:02:05 +0330

    ADD:add convert data types function

--- PreprocessData.py
+++ PreprocessData.py
@@ -1,3 +1,8 @@
 def remove_missing_values(dataset):
     dataset = dataset.drop(dataset[dataset['TotalCharges'] == " "].index)
     return dataset
+
+def convert_data_types(dataset):
+    dataset['TotalCharges'] = dataset['TotalCharges'].astype(float)
+    dataset['SeniorCitizen'] = dataset['SeniorCitizen'].astype(object)
+    return dataset
