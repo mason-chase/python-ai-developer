8685f281656f9e13a0ea9d3a08d1d590bb49be80
monirmo97 <monireh.moshavash@gmail.com>
2023-10-16 10:25:37 +0330

    ADD: add encode function and StandardScaler to main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from PreprocessData import remove_missing_values, convert_data_types
-
-
+from EncodDataset import preprocess_dataset
+from sklearn.preprocessing import StandardScaler
 #read-data
 Dataset=pd.read_csv("/content/drive/MyDrive/AI_Developer/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 Dataset.drop('customerID', axis=1, inplace=True)
@@ -9,3 +9,10 @@ Dataset.drop('customerID', axis=1, inplace=True)
 # clean-data
 cleaned_dataset = remove_missing_values(Dataset)
 cleaned_dataset = convert_data_types(cleaned_dataset)
+
+#Encode data
+Encodedata = preprocess_dataset(cleaned_dataset)
+std_scaler = StandardScaler()
+X = Encodedata.drop("Churn", axis=1)
+X = std_scaler.fit_transform(X)
+Y = Encodedata["Churn"].copy()
